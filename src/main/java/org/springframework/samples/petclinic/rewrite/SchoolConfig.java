d5ef947ccbc3c0cd7b91929ed4ef12c82e390acc
traceyyoshima <tracey.yoshima@gmail.com>
2021-09-02 09:56:41 -0700

    Added javadocs to view on the sass.

--- src/main/java/org/springframework/samples/petclinic/rewrite/SchoolConfig.java
+++ src/main/java/org/springframework/samples/petclinic/rewrite/SchoolConfig.java
@@ -6,16 +6,30 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SchoolConfig {
+        /** Purposely misaligned to cause auto-formatting.
+         * {@link}
+         * {@link DogSchool
+         * {@link DogSchool()}
+         * {@link
+         *      DogSchool}
+         */
     @Bean
     public DogSchool dogSchool() {
         return new DogSchool();
     }
 
+        /**
+         * <a href="
+         * https://...html">
+         * label</a>.
+         */
     @Bean
     public CatSchool catSchool() {
         return new CatSchool();
     }
 
+        /**
+         */
     @Bean
     @ConditionalOnBean({CatSchool.class, DogSchool.class})
     public VetSchool vetSchool() {
